dab211a19786d667960a33a0787d8e13af9c4383
Raja Asthana <rajaliving@gmail.com>
2022-05-25 18:20:20 +0530

    first commit

--- src/main/java/com/example/demo/Employee_controller.java
+++ src/main/java/com/example/demo/Employee_controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,7 +24,7 @@ public class Employee_controller {
 	}
 
 	@GetMapping(value = "/show/{id}")
-	public Employee getDetails(@PathVariable int id) {
+	public Employee getDetails(@PathVariable int id ) {
 		return empser.getDetails(id);
 	}
 
